0b1a79141fa1d77da9328e9686684233744db122
Nikolmash <mashio@yandex.ru>
2020-06-29 22:57:34 +0300

    ///

--- math/math.go
+++ math/math.go
@@ -6,12 +6,12 @@ func NthPrime(n int) int {
 	if n < 1 {
 		panic("n is less that 1")
 	}
-	smallnum := [5]int{2, 3, 5, 7, 11}
-	if n < 6 {
+	smallnum := [...]int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199}
+	if n < len(smallnum) {
 		return smallnum[n-1]
 	}
 	lnn := math.Log(float64(n))
-	upper := int(float64(n)*(lnn+math.Log(lnn))) + 3
+	upper := int(float64(n)*(lnn+math.Log(lnn))) + 20
 	arr := make([]bool, upper/2)
 	for i := 3; i*i <= upper; i += 2 {
 		for j := i * i; j <= upper; j += 2 * i {
