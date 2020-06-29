621f150431465b1356331635ca40dd3cc76f2eb0
Nikolmash <mashio@yandex.ru>
2020-06-29 08:25:50 +0300

    primes optimized

--- math/math.go
+++ math/math.go
@@ -6,25 +6,24 @@ func NthPrime(n int) int {
 	if n < 1 {
 		panic("n is less that 1")
 	}
-	smallnum := []int{2, 3, 5, 7, 11, 13}
+	smallnum := []int{2, 3, 5, 7, 11}
 	if n < 6 {
-		return smallnum[int(n)-1]
+		return smallnum[n-1]
 	}
-	upper := int(float64(n)*math.Log(float64(n)) + float64(n)*math.Log(math.Log(float64(n))))
+	upper := int(float64(n)*(math.Log(float64(n))+math.Log(float64(n))*math.Log(float64(n)))) / 2
 	arr := make([]bool, upper+10)
-
-	for i := 2; i*i < upper; i++ {
-		for j := i * i; j < upper; j += i {
-			arr[j] = true
+	for i := 3; i*i <= upper; i += 2 {
+		for j := i * i; j <= upper; j += 2 * i {
+			arr[(j-3)/2] = true
 		}
 	}
-	res := 0
+	res := 1
 	for ind, val := range arr {
-		if !val && ind > 1 {
+		if !val {
 			res += 1
 		}
 		if res == n {
-			return ind
+			return 2*ind + 3
 		}
 	}
 	panic("something wrong")
