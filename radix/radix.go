ea01c46dab87c66d32c07b821a95e715126e655e
Nikolmash <mashio@yandex.ru>
2020-07-01 02:43:45 +0300

    ...

--- radix/radix.go
+++ radix/radix.go
@@ -1,26 +1,25 @@
 package radix
 
 func Sort(data []uint64) {
-	var position int
-	var r uint8
+	var position, n_position, r int
 	sorted := make([]uint64, len(data))
-	copy_data := make([]uint64, len(data))
-	copy(copy_data, data)
 	for i := 0; i < 8; i++ {
 		C := [256]int{}
-		for j := range copy_data {
-			r = uint8(copy_data[j])
+		for j := range data {
+			r = int((data[j] >> (i * 8)) & 0b11111111)
 			C[r]++
 		}
 		position = C[0]
+		C[0] = 0
 		for j := 1; j < 256; j++ {
-			position = C[j]
+			n_position = C[j]
 			C[j] = C[j-1] + position
+			position = n_position
 		}
 		for j := range data {
-			r = uint8(copy_data[j])
-			sorted[C[r]-1] = data[j]
-			copy_data[j] >>= 8
+			r = int((data[j] >> (i * 8)) & 0b11111111)
+			sorted[C[r]] = data[j]
+			C[r]++
 		}
 		copy(data, sorted)
 	}
